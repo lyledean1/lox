956a89148d350e3d3d084d78e534e9da4a553608
Lyle Dean <lylebd1@gmail.com>
2021-11-07 21:16:06 +0000

    adding hash values

--- clox/value.h
+++ clox/value.h
@@ -3,10 +3,14 @@
 
 #include "common.h"
 
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
+
 typedef enum {
     VAL_BOOL,
     VAL_NIL,
     VAL_NUMBER,
+    VAL_OBJ,
 } ValueType;
 
 typedef struct {
@@ -14,6 +18,7 @@ typedef struct {
     union {
         bool boolean;
         double number;
+        Obj* obj;
     } as;
 } Value;
 
@@ -23,10 +28,12 @@ typedef struct {
 
 #define AS_BOOL(value)  ((value).as.boolean)
 #define AS_NUMBER(value)  ((value).as.number)
+#define AS_OBJ(value) ((value).as.obj)
 
 #define BOOL_VAL(value) ((Value) {VAL_BOOL, {.boolean = value}})
 #define NIL_VAL ((Value) {VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value) {VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object) ((Value) {VAL_OBJ, {.obj = (Obj*)object}})
 
 typedef struct {
     int capacity;
