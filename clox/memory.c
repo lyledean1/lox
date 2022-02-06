7512bcb9be77cb2f46e1ae02254d92d0b8ccb3ac
Lyle Dean <lylebd1@gmail.com>
2022-02-06 17:08:26 +0000

    Adding garbage collection

--- clox/memory.c
+++ clox/memory.c
@@ -1,9 +1,20 @@
 #include <stdlib.h>
+#include "compiler.h"
 #include "memory.h"
 #include "vm.h"
 
+#ifdef DEBUG_LOG_GC
+#include <stdio.h>
+#include "debug.h"
+#endif
+
 void* reallocate(void* pointer, size_t oldSize, size_t newSize)
 {
+    if (newSize > oldSize) {
+        #ifdef DEBUG_STRESS_GC
+            collectGarbage();
+        #endif
+    }
     if (newSize == 0)
     {
         free(pointer);
@@ -16,7 +27,32 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize)
     return result;
 }
 
+void markObject(Obj* object) {
+    if (object == NULL) return;
+    #ifdef DEBUG_LOG_GC
+        printf("%p mark \n", (void*)object);
+        printValue(OBJ_VAL(object));
+        printf("\n");
+    #endif 
+    object->isMarked = true;
+
+    if (vm.grayCapacity < vm.grayCount + 1) {
+        vm.grayCapacity = GROW_CAPACITY(vm.grayCapacity);
+        vm.grayStack = (Obj**)realloc(vm.grayStack, sizeof(Obj*) * vm.grayCapacity);
+    }
+
+    vm.grayStack[vm.grayCount++] = object;
+    if (vm.grayStack == NULL) exit(1);
+}
+
+void markValue(Value value) {
+    if (IS_OBJ(value)) markObject(AS_OBJ(value));
+}
+
 static void freeObject(Obj* object) {
+    #ifdef DEBUG_LOG_GC
+        printf("%p free type %d\n", (void*)object, object->type);
+    #endif 
     switch(object->type) {
         case OBJ_CLOSURE: {
             ObjClosure* closure = (ObjClosure*)object;
@@ -47,6 +83,35 @@ static void freeObject(Obj* object) {
     }
 }
 
+static void markRoots() {
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+        markValue(*slot);
+    }
+
+    for (int i = 0; i < vm.frameCount; i++) {
+        markObject((Obj*)vm.frames[i].closure);
+    }
+
+    for (ObjUpvalue* upvalue = vm.openUpvalues; upvalue != NULL; upvalue = upvalue->next) {
+        markObject((Obj*)upvalue);
+    }
+
+    markTable(&vm.globals);
+    markCompilerRoots();
+}
+
+void collectGarbage() {
+    #ifdef DEBUG_LOG_GC
+        printf("-- gc begin\n");
+    #endif
+
+    markRoots();
+
+    #ifdef DEBUG_LOG_GC
+        printf("-- gc end\n");
+    #endif
+}
+
 
 void freeObjects() {
     Obj* object = vm.objects;
@@ -55,4 +120,5 @@ void freeObjects() {
         freeObject(object);
         object = next;
     }
+    free(vm.grayStack);
 }
